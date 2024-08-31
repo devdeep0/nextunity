455e1d21725aa15f0793fba86189bd59d0c18280
Deepanshu <141805113+devdeep0@users.noreply.github.com>
2024-09-01 00:33:45 +0530

    de

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div className='h-screen w-full bg-red-500'>page</div>
+    <div className='h-screen w-full bg-orange-50 flex items-center justify-center'>main</div>
   )
 }
 
