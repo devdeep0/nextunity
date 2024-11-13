2e087682e13c77326e8257ad995e35174f3eefe9
MilitantNoob <rohitkumartharpyal7070@gmail..com>
2024-11-13 11:24:23 +0530

    deleted builds changed name

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -172,7 +172,7 @@ const GameSelectionUI : React.FC<GameSelectionUIProps> = ({ isLoading, selectedG
                    </div>
 
           
-               {/* game 5 = fruit ninja  */}
+               {/* game 5 = Trailblazer */}
                    <div className="h-[57px] w-full rounded-[14px] flex items-center justify-center gap-4 bg-transparent shrink-0 bg-center " >
                      <div className='h-full w-full flex items-center gap-4'>
                        <div className='h-full w-[57px] bg-center bg-cover rounded-[14px] border-2 broder-white'style={{backgroundImage: `url('/gameimg/TrailblazerTaiko.png')`}}></div>
